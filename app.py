98471cc0c6ea5da7f1cff332de7d3ffd19cc3d2f
PatriciaG-Dev <lipagu.dev@gmail.com>
2026-02-19 12:31:27 -0500

    Fix puerto render

--- app.py
+++ app.py
@@ -17,6 +17,5 @@ def add_task():
         tasks.append(task)
     return redirect('/')
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
